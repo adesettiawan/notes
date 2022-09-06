4965b256119310fb712c98a1233a8fa32bf05600
adesettiawan <adesetiawan0675@gmail.com>
2022-09-06 20:19:02 +0700

    fixing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Notes from "./components/notes/Notes";
 
-// import style
+// import style css
 import "./styles/style.css";
 
 const root = createRoot(document.getElementById("root"));
